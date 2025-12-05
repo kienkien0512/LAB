a6bbed06b6bb0de6fe41d559200885d311f0eba8
kienkien0512 <kienlttph51115@gmail.com>
2025-12-05 12:57:46 +0700

    update
    Remove commented code related to token and user storage.

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -17,7 +17,6 @@ function LoginPage() {
     try {
       const res = await axios.post("http://localhost:3000/login", user);
 
-      // Lưu token + user
       localStorage.setItem("token", res.data.accessToken);
       localStorage.setItem("user", JSON.stringify(res.data.user));
 
